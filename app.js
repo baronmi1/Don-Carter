c7f36ced92d86bc364a962a834cf29363ea30cf5
Kennedy Chidi <kennedychididev@gmail.com>
2023-03-30 19:00:33 +0100

    good

--- app.js
+++ app.js
@@ -22,22 +22,22 @@ const accountRouter = require("./routes/accountRoutes");
 const aboutRouter = require("./routes/aboutRoutes");
 dotenv.config({ path: "./config.env" });
 
-const chokidar = require("chokidar");
+// const chokidar = require("chokidar");
 
-const watcher = chokidar.watch(
-  "/app/controllers/transactionController.js",
-  "/app/utils/email.js",
-  "/app/app.js",
-  {
-    ignored: /[\/\\]\./, // ignore dotfiles
-    persistent: true, // keep the process running
-  }
-);
+// const watcher = chokidar.watch(
+//   "/app/controllers/transactionController.js",
+//   "/app/utils/email.js",
+//   "/app/app.js",
+//   {
+//     ignored: /[\/\\]\./, // ignore dotfiles
+//     persistent: true, // keep the process running
+//   }
+// );
 
-watcher
-  .on("add", (path) => console.log(`File ${path} has been added`))
-  .on("change", (path) => console.log(`File ${path} has been changed`))
-  .on("unlink", (path) => console.log(`File ${path} has been removed`));
+// watcher
+//   .on("add", (path) => console.log(`File ${path} has been added`))
+//   .on("change", (path) => console.log(`File ${path} has been changed`))
+//   .on("unlink", (path) => console.log(`File ${path} has been removed`));
 
 const app = express();
 const server = require("http").createServer(app);
