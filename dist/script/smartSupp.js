bc7b5f893376a740aced27047d75313fb80472f3
Kennedy Chidi <kennedychididev@gmail.com>
2023-04-25 12:41:32 +0100

    new commit

---
+++ dist/script/smartSupp.js
@@ -0,0 +1,18 @@
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = "e7e8f4afc8e24b8f61e825ce6efb067a833d3d15";
+window.smartsupp ||
+  (function (d) {
+    var s,
+      c,
+      o = (smartsupp = function () {
+        o._.push(arguments);
+      });
+    o._ = [];
+    s = d.getElementsByTagName("script")[0];
+    c = d.createElement("script");
+    c.type = "text/javascript";
+    c.charset = "utf-8";
+    c.async = true;
+    c.src = "https://www.smartsuppchat.com/loader.js?";
+    s.parentNode.insertBefore(c, s);
+  })(document);
